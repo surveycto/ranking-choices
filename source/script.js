c33a640e0f0dda403c192312b063026ae0f06421
Max S. Haberman <max@surveycto.com>
2020-05-27 20:51:05 -0400

    Moved functions to bottom

--- source/script.js
+++ source/script.js
@@ -1,125 +1,69 @@
-/* class fPChoice {
-  constructor(index, value, label) {
+class Choice {
+  constructor (value, index, label, image) {
     this.CHOICE_INDEX = index
     this.CHOICE_VALUE = value
     this.CHOICE_LABEL = label
+    this.CHOICE_IMAGE = image
   }
 }
 
-fieldProperties = {
+var fieldProperties = {
   CHOICES: [
-    new fPChoice(0, 0, 'Choice 1'),
-    new fPChoice(1, 1, 'Choice 2'),
-    new fPChoice(2, 2, 'Choice 3'),
+    new Choice(0, 0, 'Choice 1'),
+    new Choice(1, 1, 'Choice 2'),
+    new Choice(2, 2, 'Choice 3')
   ],
-  METADATA: '1 2 0',
-  LABEL: 'Hi!',
-  HINT: 'Lo!',
+  METADATA: '',
+  LABEL: 'This is a label',
+  HINT: 'This is a hint',
   PARAMETERS: [
     {
-      key: 'allowdef',
+      key: 'parameter1',
       value: '1'
     },
     {
-      key: 'numbers',
-      value: '0'
+      key: 'parameter2',
+      value: '2'
     }
-  ]
+  ],
+  FIELDTYPE: 'select_multiple',
+  APPEARANCE: '',
+  LANGUAGE: 'english'
 }
 
-function setAnswer(ans) {
+function setAnswer (ans) {
   console.log('Set answer to: ' + ans)
 }
 
-function setMetaData(string) {
+function setMetaData (string) {
   fieldProperties.METADATA = string
 }
 
-function getMetaData() {
+function getMetaData () {
   return fieldProperties.METADATA
 }
 
-function getPluginParameter(param){
-  for(let p of fieldProperties.PARAMETERS){
-    let key = p.key
-    if(key == param){
-      return p.value
-    }
-  }
-  return
+function getPluginParameter (param) {
+  const parameters = fieldProperties.PARAMETERS
+  if (parameters != null) {
+    for (const p of fieldProperties.PARAMETERS) {
+      const key = p.key
+      if (key == param) {
+        return p.value
+      } // End IF
+    } // End FOR
+  } // End IF
 }
 
-class Choice {
-  constructor(index, label) {
-    this.index = index
-    this.label = label
-  }
+function goToNextField () {
+  console.log('Skipped to next field')
 }
-
-document.body.classList.add('android-collect')
+// document.body.classList.add('android-collect')
 // Above for testing only */
-/* var testDiv = document.querySelector('#testing')
-function testing(text){
-  testDiv.innerHTML += text + '<br>\n'
-} */
 
 /* global setAnswer, fieldProperties, getMetaData, setMetaData, getPluginParameter, Sortable, setFocus */
 
-function dispChoices (orderStart) {
-  if (orderStart == null) { // If empty (no order set yet), then should show in original order
-    orderStart = []
-    for (let i = 0; i < numChoices; i++) {
-      orderStart.push(choices[i].CHOICE_VALUE)
-    }
-    orderStartSpaces = orderStart.join(' ')
-  }
-  // Used to display the choices in the correct order
-  for (let r = 0; r < numChoices; r++) {
-    const choiceValue = orderStart[r]
-    const thisChoice = choicesObj[choiceValue]
-    const choiceLabel = unEntity(thisChoice.label)
-    let choiceItem = '<li class="list-item" data-id="' + choiceValue + '">'
 
-    if (useNumbers == 1) {
-      choiceItem += '<span id="rank" dir="auto"></span>. '
-    }
-
-    choiceItem += choiceLabel + '</li>'
-    choicesHolder.innerHTML += choiceItem
-  } // End FOR to display choices in the correct order
-}
-
-function clearAnswer () {
-  dispChoices()
-  setAnswer()
-}
-
-function handleConstraintMessage (message) {
-  formGroup.classList.add('has-error')
-  controlMessage.innerHTML = message
-  setFocus()
-}
-
-function handleRequiredMessage (message) {
-  handleConstraintMessage(message)
-}
-
-function unEntity (str) {
-  if (str == null) {
-    return null
-  } else {
-    return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-  }
-}
-
-function setRanks () {
-  if (useNumbers == 1) {
-    rankSpans = choicesHolder.querySelectorAll('#rank')
-    for (let r = 0; r < numChoices; r++) {
-      rankSpans[r].innerHTML = (r + 1)
-    }
-  }
-}
 
 var label = document.querySelector('.label')
 var hint = document.querySelector('.hint')
@@ -190,3 +134,59 @@ var getOrder = function () {
 document.addEventListener('mousedown', function () { // This removes the blue border during moveing. Otherwise, it appears in seemingly-random spots. It is removed when the Sortable is done.
   choicesHolder.classList.remove('hovering')
 })
+
+function dispChoices (orderStart) {
+  if (orderStart == null) { // If empty (no order set yet), then should show in original order
+    orderStart = []
+    for (let i = 0; i < numChoices; i++) {
+      orderStart.push(choices[i].CHOICE_VALUE)
+    }
+    orderStartSpaces = orderStart.join(' ')
+  }
+  // Used to display the choices in the correct order
+  for (let r = 0; r < numChoices; r++) {
+    const choiceValue = orderStart[r]
+    const thisChoice = choicesObj[choiceValue]
+    const choiceLabel = unEntity(thisChoice.label)
+    let choiceItem = '<li class="list-item" data-id="' + choiceValue + '">'
+
+    if (useNumbers == 1) {
+      choiceItem += '<span id="rank" dir="auto"></span>. '
+    }
+
+    choiceItem += choiceLabel + '</li>'
+    choicesHolder.innerHTML += choiceItem
+  } // End FOR to display choices in the correct order
+}
+
+function clearAnswer () {
+  dispChoices()
+  setAnswer()
+}
+
+function handleConstraintMessage (message) {
+  formGroup.classList.add('has-error')
+  controlMessage.innerHTML = message
+  setFocus()
+}
+
+function handleRequiredMessage (message) {
+  handleConstraintMessage(message)
+}
+
+function unEntity (str) {
+  if (str == null) {
+    return null
+  } else {
+    return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+  }
+}
+
+function setRanks () {
+  if (useNumbers == 1) {
+    rankSpans = choicesHolder.querySelectorAll('#rank')
+    for (let r = 0; r < numChoices; r++) {
+      rankSpans[r].innerHTML = (r + 1)
+    }
+  }
+}
